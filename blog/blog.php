50f2ebc40dc72e406af429692ba8d2a6e8a9aad1
v-julio <vjrojas85@misena.edu.co>
2019-09-23 17:36:46 -0500

    changge title

--- blog/blog.php
+++ blog/blog.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Formulario</title>
+    <title>Fuerce_Blog</title>
   </head>
   <body>
     <nav class ="navbar navbar-dark bg-dark">
